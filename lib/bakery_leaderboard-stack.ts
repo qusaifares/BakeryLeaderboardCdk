c66e1ef579b9fa88946f886a07a044e87e411c89
Qusai Fares <qusaifares@gmail.com>
2023-10-08 10:34:54 -0700

    Add data, compute and orchestration resources

--- lib/bakery_leaderboard-stack.ts
+++ lib/bakery_leaderboard-stack.ts
@@ -1,20 +1,78 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/prefer-default-export */
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class BakeryLeaderboardStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a new VPC or use an existing one
+    const vpc = new ec2.Vpc(this, 'MyVpc');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BakeryLeaderboardQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-    const summonerSourceLambda = new lambda.NodejsFunction(this, 'SummonerSourceLambda', {});
+    // Define the Aurora Database cluster
+    const auroraCluster = new rds.DatabaseCluster(this, 'BakeryLeaderboardDatabase', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_15_3,
+      }),
+      credentials: rds.Credentials.fromGeneratedSecret('admin'), // auto-generate the password
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
+        vpc,
+      },
+      defaultDatabaseName: 'BakeryLeaderboardDatabase',
+    });
+
+    // DynamoDB
+    const matchesDynamoDbTable = new dynamodb.Table(this, 'MatchesDynamoDbTable', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: 'Matches',
+    });
+
+    const summonerSourceLambda = new lambda.NodejsFunction(this, 'SummonerSourceLambda', {
+      entry: '/node_modules/BakeryLeaderboardSummonerSourceLambda/src/index.ts',
+    });
+
+    const sourceMatchesLambda = new lambda.NodejsFunction(this, 'SourceMatchesLambda', {
+    });
+
+    const getMatchIdsForSummonerLambda = new lambda.NodejsFunction(this, 'GetMatchIdsForSummonerLambda', {
+    });
+
+    const summonersToFetchMatchesForQueue = new sqs.Queue(this, 'SummonersToFetchMatchesForQueue', {
+    });
+
+    const fetchMatchQueue = new sqs.Queue(this, 'FetchMatchQueue', {
+    });
+
+    fetchMatchQueue.grantSendMessages(getMatchIdsForSummonerLambda);
+    summonersToFetchMatchesForQueue.grantSendMessages(sourceMatchesLambda);
+
+    // Step Functions
+    const checkIfMatchExistsLambda = new lambda.NodejsFunction(this, 'CheckIfMatchExistsLambda', {});
+    const fetchMatchAndInsertLambda = new lambda.NodejsFunction(this, 'FetchMatchAndInsertLambda', {});
+
+    const checkIfMatchExistsTask = new tasks.LambdaInvoke(this, 'CheckIfMatchExistsInvocation', {
+      lambdaFunction: checkIfMatchExistsLambda,
+    });
+
+    const fetchMatchAndInsertTask = new tasks.LambdaInvoke(this, 'FetchMatchAndInsertInvocation', {
+      lambdaFunction: fetchMatchAndInsertLambda,
+    });
+
+    const doesMatchExistChoiceState = new sfn.Choice(this, 'DoesMatchExistChoice')
+      .when(sfn.Condition.booleanEquals('$.doesMatchExist', false), fetchMatchAndInsertTask);
+
+    const matchSourceDefinition = checkIfMatchExistsTask.next(doesMatchExistChoiceState);
   }
 }
